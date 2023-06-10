297ccf7767f2e426d8d1b0ce36a3c14b1994eefe
LTree0703 <108115115+LTree0703@users.noreply.github.com>
2023-06-10 13:42:36 +0800

    Homepage design completed

---
+++ lib/grade.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GradeForm extends StatefulWidget {
+  const GradeForm({super.key});
+
+  @override
+  State<GradeForm> createState() => _GradeFormState();
+}
+
+class _GradeFormState extends State<GradeForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:gpa_calculator/gpa.dart';
+import 'package:gpa_calculator/grade.dart';
 
-void main() => runApp(const LaunchApp());
+void main() {
+  runApp(const LaunchApp());
+}
 
 class LaunchApp extends StatelessWidget {
   const LaunchApp({super.key});
@@ -8,28 +12,140 @@ class LaunchApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'GPA Calculator',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color.fromARGB(255, 18, 50, 98)),
-        useMaterial3: true,
-      ),
+      title: 'Yeet',
+      theme:
+          ThemeData(primarySwatch: Colors.indigo, fontFamily: 'JetBrainsMono'),
       debugShowCheckedModeBanner: false,
       home: const HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Material(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color.fromARGB(255, 18, 50, 98),
+                Color.fromARGB(150, 18, 50, 98),
+                Color.fromARGB(120, 18, 50, 98),
+                Color.fromARGB(190, 18, 50, 98),
+                Color.fromARGB(220, 18, 50, 98),
+                Color.fromARGB(255, 18, 50, 98),
+              ]),
+        ),
+        child: Column(
+          children: [
+            const Flexible(child: SizedBox(height: 230)),
+            const Text(
+              'Uni\nQuitter',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white60,
+                // decoration: TextDecoration.lineThrough,
+                fontSize: 64,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(50, 30, 50, 0),
+              child: const Text(
+                '\'People go through three stages: birth, get an F, perish.\' (a random uni fm, 2023)',
+                style: TextStyle(
+                  color: Colors.white60,
+                ),
+              ),
+            ),
+            const Flexible(child: SizedBox(height: 177)),
+            FilledButton.tonal(
+              style: const ButtonStyle(
+                fixedSize: MaterialStatePropertyAll(Size(307, 76)),
+              ),
+              onPressed: () => const GPAForm(),
+              // <a href="https://www.flaticon.com/free-icons/education" title="education icons">Education icons created by Freepik - Flaticon</a>
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 26, 20, 26),
+                    child: Image.asset('assets/gpa.png',
+                        color: Colors.white, width: 24, height: 24),
+                  ),
+                  const Flexible(
+                    child: Text(
+                      'GPA Calculator',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Flexible(child: SizedBox(height: 42)),
+            FilledButton.tonal(
+              style: const ButtonStyle(
+                fixedSize: MaterialStatePropertyAll(Size(307, 76)),
+              ),
+              onPressed: () => const GradeForm(),
+              // <a href="https://www.flaticon.com/free-icons/grade" title="grade icons">Grade icons created by Freepik - Flaticon</a>
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 26, 20, 26),
+                    child: Image.asset('assets/grade.png',
+                        color: Colors.white, width: 24, height: 24),
+                  ),
+                  const Flexible(
+                    child: Text(
+                      'Grade Calculator',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 42),
+            // <a href="https://www.flaticon.com/free-icons/settings" title="settings icons">Settings icons created by Freepik - Flaticon</a>
+            // <a href="https://www.flaticon.com/free-icons/about" title="about icons">About icons created by Tempo_doloe - Flaticon</a>
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: const ButtonStyle(
+                      shape: MaterialStatePropertyAll(CircleBorder(
+                          side: BorderSide(style: BorderStyle.none))),
+                      fixedSize: MaterialStatePropertyAll(Size(30, 30))),
+                  onPressed: () {},
+                  child: Image.asset('assets/setting.png',
+                      color: Colors.white, width: 24, height: 24),
+                ),
+                const SizedBox(width: 52),
+                ElevatedButton(
+                  style: const ButtonStyle(
+                      shape: MaterialStatePropertyAll(CircleBorder(
+                          side: BorderSide(style: BorderStyle.none))),
+                      fixedSize: MaterialStatePropertyAll(Size(30, 30))),
+                  onPressed: () {},
+                  child: Image.asset('assets/info.png',
+                      color: Colors.white, width: 24, height: 24),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
